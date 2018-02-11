ff52bdcf548d2a1d2e3d141d6160a90ec4ab9231
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-02-11 15:32:38 +0530

    Create basic UI

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCardModule
   ],
   exports: [
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCardModule
   ],
   declarations: []
 })
