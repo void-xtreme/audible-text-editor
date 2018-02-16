02920ffccaf1213f5ef76ca3ff9bf16ed732d61e
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-02-17 02:43:35 +0530

    Modify user interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatRadioModule} from '@angular/material';
-import {FormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatInputModule} from '@angular/material/input';
-import {FestivalService} from './services/festival.service';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatRadioModule, MatIconModule } from '@angular/material';
+import { FormsModule } from '@angular/forms'; 
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule, MatToolbarRow } from '@angular/material/toolbar';
+import { FestivalService } from './services/festival.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 
 @NgModule({
@@ -27,7 +30,10 @@ import {HttpClientModule} from '@angular/common/http';
     MatRadioModule,
     MatButtonModule,
     MatDividerModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [
     FestivalService
